58a547ae30165fd13c4fa07eff18571f12d37922
Alexio Ju <with6928?@gmail.com>
2021-06-05 23:57:51 +0900

    Initial commit

---
+++ src/test/java/com/example/vuespringjstest/VueSpringJsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuespringjstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSpringJsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
